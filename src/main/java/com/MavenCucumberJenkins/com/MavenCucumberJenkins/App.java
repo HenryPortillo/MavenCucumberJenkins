e3ef971d28783a02a51a23b8ef4d721d5e294b49
HenryPortillo <portillo.henry14@gmail.com>
2018-12-29 13:43:17 -0500

    initial commit

---
+++ src/main/java/com/MavenCucumberJenkins/com/MavenCucumberJenkins/App.java
@@ -0,0 +1,13 @@
+package com.MavenCucumberJenkins.com.MavenCucumberJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
